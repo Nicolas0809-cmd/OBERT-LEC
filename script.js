a0d7c29f9ea10965861914152ab5ea3855beff9d
Nicolas0809-cmd <obert.nicolas@icloud.com>
2025-03-27 22:43:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-    const fadeElements = document.querySelectorAll('.fade-in');
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-            }
-        });
-    });
-    fadeElements.forEach(el => observer.observe(el));
-});
+document.addEventListener('DOMContentLoaded', () => { document.querySelectorAll('.fade-in').forEach(el => el.style.opacity = 1); });
